65a57196b7a9fabfcea5848af99d84b70dcf4101
Chris Walquist <cwalquist@drw.com>
2020-11-10 16:51:05 -0600

    Add a (poorly designed) delete route

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ cfg = load_config(join(app.root_path, '../shared/config.yml'))
 @app.route('/', methods=['GET'])
 def show_tags():
     tags = Tag.all()
-    tags_html = '\n'.join(list(map(lambda x: x.name + "<br>", tags)))
+    tags_html = '\n'.join(list(map(lambda x: "<a href=\"/tags/%s\">%s</a><br>" % (x.name, x.name), tags)))
     form_html = "<form action=\"/tags\" method=\"POST\"><label>Enter a tag: </label><input name=\"tag-name\"></form>"
     #embed()
     return "<h1>The Ultimate Tag Manager</h1><h1>Hello World!</h1><img src=\"%s\" style=\"width:300px\"><div>%s</div><div>%s</div>" % (cfg['awesome_image'],tags_html, form_html)
@@ -26,5 +26,14 @@ def add_tag():
     tag = Tag.where('tag', new_tag).first()
     if tag is None:
         tag = Tag.create(name=new_tag)
+
+    return redirect('/')
+
+# GET is not the recommended way to implement DELETE, but oh well...
+@app.route('/tags/<tag>', methods=['GET'])
+def remove_tag(tag):
+    tag_to_remove = Tag.where('name', tag).first()
+    if tag_to_remove is not None:
+        tag_to_remove.delete()
 
     return redirect('/')
