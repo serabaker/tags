badd54fa08db244969a7474b62062372390f92bc
Chris Walquist <cwalquist@drw.com>
2020-11-13 11:08:38 -0600

    Use jQuery to intercept click event on <a> element with 'delete_link' class, and send a DELETE request

--- app.py
+++ app.py
@@ -12,13 +12,27 @@ from tag import Tag # After app_init(), which sets db for the model
 
 cfg = load_config(join(app.root_path, '../shared/config.yml'))
 
+delete_js = '''
+<script type="text/javascript" src="static/js/lib/jquery-3.5.1.js"></script>
+<script type="text/javascript">
+$('.delete_link').on('click', function(e) {
+  var url = "http://localhost:5555" + $(this).attr('href');
+  var xhr = new XMLHttpRequest();
+  xhr.open("DELETE", url, true);
+  xhr.send(null);
+  window.location.href='/';  // Redirect
+  return false;
+});
+</script>
+'''
+
 @app.route('/', methods=['GET'])
 def show_tags():
     tags = Tag.all()
-    tags_html = '\n'.join(list(map(lambda x: "<a href=\"/tags/%s\">%s</a><br>" % (x.name, x.name), tags)))
+    tags_html = '\n'.join(list(map(lambda x: "<a class=\"delete_link\" href=\"/tags/%s\">%s</a><br>" % (x.name, x.name), tags)))
     form_html = "<form action=\"/tags\" method=\"POST\"><label>Enter a tag: </label><input name=\"tag-name\"></form>"
     #embed()
-    return "<h1>The Ultimate Tag Manager</h1><h1>Hello World!</h1><img src=\"%s\" style=\"width:300px\"><div>%s</div><div>%s</div>" % (cfg['awesome_image'],tags_html, form_html)
+    return "<h1>The Ultimate Tag Manager</h1><h1>Hello World!</h1><img src=\"%s\" style=\"width:300px\"><div>%s</div><div>%s</div>%s" % (cfg['awesome_image'],tags_html, form_html, delete_js)
 
 @app.route('/tags', methods=['POST'])
 def add_tag():
@@ -29,11 +43,10 @@ def add_tag():
 
     return redirect('/')
 
-# GET is not the recommended way to implement DELETE, but oh well...
-@app.route('/tags/<tag>', methods=['GET'])
+@app.route('/tags/<tag>', methods=['DELETE'])
 def remove_tag(tag):
     tag_to_remove = Tag.where('name', tag).first()
     if tag_to_remove is not None:
         tag_to_remove.delete()
 
-    return redirect('/')
+    return '', 204  # No need to return any content, since Javascript will redirect anyway
